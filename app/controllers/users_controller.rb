59a4a27e72b82299ab07b79c0ffb6d7f3cdfeef0
Sirapope.c <sirapope.c@gmail.com>
2024-09-05 14:24:26 +0700

    feat(controller,view): add partial form + update controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,46 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def register
+    @user = User.new
   end
 
   def create
-    # Here you would typically save the user data
-    # For now, we'll just redirect back to the form
-    redirect_to users_new_path, notice: 'User registered successfully!'
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_register_path, notice: 'User registered successfully!'
+    else
+      render 'register', status: :unprocessable_entity
+    end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: 'User was successfully updated.'
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_url, notice: 'User was successfully deleted.'
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :birthday, :gender, :email, :phone_number, :subject)
+  end
+
 end
